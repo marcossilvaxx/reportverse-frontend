0ac995c00c55f5347d61e444241d071553551bba
Marcos Silva <vinicius_marcosmartins@hotmail.com>
2022-07-30 20:18:39 -0300

    feat: finalizando integração das telas de admin, criacao e mapa com o back

--- reportverse-front/src/router/Routes.jsx
+++ reportverse-front/src/router/Routes.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Judgement from '../pages/admin/judgement';
+import Reports from '../pages/admin/reports';
 import Home from '../pages/home/Index';
 import LoginPage from '../pages/loginPage/loginPageComponent';
 import Map from '../pages/map';
@@ -9,11 +11,13 @@ import RegisterPage from '../pages/registerPage/registerPageComponent';
 function Routes() {
   return (
     <Switch>
-      <Route exact path="/" render={LoginPage} />
-      <Route exact path="/register" render={RegisterPage} />
-      <Route exact path="/home" render={Home} />
-      <Route exact path="/postar" render={NewPost} />
-      <Route exact path="/mapa" render={Map} />
+      <Route exact path="/" component={LoginPage} />
+      <Route exact path="/register" component={RegisterPage} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/postar" component={NewPost} />
+      <Route exact path="/mapa" component={Map} />
+      <Route exact path="/admin" component={Reports} />
+      <Route exact path="/admin/report/:id" component={Judgement} />
     </Switch>
   );
 }
---
+++ reportverse-front/src/utils/formatReport.js
@@ -0,0 +1,14 @@
+const formatReport = (report) => ({
+  id: report.id,
+  lat: report.latitude,
+  lon: report.longitude,
+  data: {
+    description: report.description,
+    name: report.authorName,
+    time: new Date(report.creationDate).toLocaleTimeString("pt-BR"),
+    date: new Date(report.creationDate).toLocaleDateString("pt-BR"),
+    isResolved: report.isResolved
+  }
+});
+
+export default formatReport;
